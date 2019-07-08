df85a15880df6e99d3dd03e70546eac508c56cea
sunven <604164227@qq.com>
2019-07-09 00:12:00 +0800

    fix error

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,122 +1,55 @@
-const path = require('path')
-// NOTE 在 sass 中通过别名（@ 或 ~）引用需要指定路径
-const sassImporter = function(url) {
-  if (url[0] === '~' && url[1] !== '/') {
-    return {
-      file: path.resolve(__dirname, '..', 'node_modules', url.substr(1))
-    }
+import Taro, { Component } from "@tarojs/taro";
+import { View, Text, Swiper, SwiperItem, Image } from "@tarojs/components";
+import api from "../../utils/api";
+import "./index.scss";
+
+export default class Index extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      topArr: []
+    };
+  }
+  config = {
+    navigationBarTitleText: "首页"
+  };
+
+  componentWillMount() {
   }
 
-  const reg = /^@styles\/(.*)/
-  return {
-    file: reg.test(url) ? path.resolve(__dirname, '..', 'src/styles', url.match(reg)[1]) : url
+  componentDidMount() {
+    api.get("nowplaying").then(res => {
+      console.log(res.data.entries.slice(0, 3));
+      this.setState({topArr:res.data.entries.slice(0, 3)});
+    });}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    let { topArr } = this.state;
+    return (
+      <View className='home__banner'>
+        <Swiper
+          className="home__banner"
+          indicatorColor="#999"
+          indicatorActiveColor="#333"
+          circular
+          indicatorDots
+          autoplay
+        >
+          {topArr.map(item => {
+            return (
+              <SwiperItem key={item.id}>
+                <Image src={item.images.small1} />
+              </SwiperItem>
+            );
+          })}
+        </Swiper>
+      </View>
+    );
   }
 }
-
-const config = {
-  projectName: "taro-douban",
-  date: "2019-7-8",
-  designWidth: 750,
-  deviceRatio: {
-    "640": 2.34 / 2,
-    "750": 1,
-    "828": 1.81 / 2
-  },
-  sourceRoot: "src",
-  outputRoot: "dist",
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        [
-          "env",
-          {
-            modules: false
-          }
-        ]
-      ],
-      plugins: [
-        "transform-decorators-legacy",
-        "transform-class-properties",
-        "transform-object-rest-spread"
-      ]
-    },
-    sass: {
-      importer: sassImporter
-    }
-  },
-  defineConstants: {},
-  alias: {
-    "@actions": path.resolve(__dirname, "..", "src/actions"),
-    "@assets": path.resolve(__dirname, "..", "src/assets"),
-    "@components": path.resolve(__dirname, "..", "src/components"),
-    "@constants": path.resolve(__dirname, "..", "src/constants"),
-    "@reducers": path.resolve(__dirname, "..", "src/reducers"),
-    "@styles": path.resolve(__dirname, "..", "src/styles"),
-    "@utils": path.resolve(__dirname, "..", "src/utils")
-  },
-  copy: {
-    patterns: [],
-    options: {}
-  },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ["last 3 versions", "Android >= 4.1", "ios >= 8"]
-          }
-        },
-        pxtransform: {
-          enable: true,
-          config: {}
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240 // 设定转换尺寸上限
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: "module", // 转换模式，取值为 global/module
-            generateScopedName: "[name]__[local]___[hash:base64:5]"
-          }
-        }
-      }
-    }
-  },
-  h5: {
-    publicPath: "/",
-    staticDirectory: "static",
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ["last 3 versions", "Android >= 4.1", "ios >= 8"]
-          }
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: "module", // 转换模式，取值为 global/module
-            generateScopedName: "[name]__[local]___[hash:base64:5]"
-          }
-        }
-      }
-    },
-    sassLoaderOption: {
-      importer: sassImporter
-    }
-  }
-};
-
-module.exports = function(merge) {
-  if (process.env.NODE_ENV === "development") {
-    return merge({}, config, require("./dev"));
-  }
-  return merge({}, config, require("./prod"));
-};
